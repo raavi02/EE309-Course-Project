f9fb65d5eb01547329c8b66c3c25ac6c058f12eb
Srishti Sharma <81479041+srishtis19@users.noreply.github.com>
2022-04-17 18:30:17 +0530

    encoding change T2

--- T2.vhd
+++ T2.vhd
@@ -7,7 +7,7 @@ entity Temp2 is
 	port (d2 : IN STD_LOGIC_VECTOR(15 downto 0);
 				Mem_D : IN STD_LOGIC_VECTOR(15 downto 0);
 				SE : IN STD_LOGIC_VECTOR(15 downto 0);
-				current_state : IN STD_LOGIC_VECTOR(5 downto 0);
+				current_state : IN STD_LOGIC_VECTOR(4 downto 0);
 
 				output : OUT STD_LOGIC_VECTOR(15 downto 0);
 				t2_write : OUT STD_LOGIC);
@@ -15,14 +15,14 @@ end Temp2;
 
 
 architecture beh of Temp2 is
-  constant S3: STD_LOGIC_VECTOR(5 downto 0) := "000011";
-  constant S6: STD_LOGIC_VECTOR(5 downto 0) := "000110";
-  constant S14: STD_LOGIC_VECTOR(5 downto 0) := "001110";
-  constant S19: STD_LOGIC_VECTOR(5 downto 0) := "010011";
-  constant S21: STD_LOGIC_VECTOR(5 downto 0) := "010101";
-  constant S24: STD_LOGIC_VECTOR(5 downto 0) := "011000";
-  constant S27: STD_LOGIC_VECTOR(5 downto 0) := "011011";
-  constant S31: STD_LOGIC_VECTOR(5 downto 0) := "011111";
+  constant S3 : std_logic_vector(4 downto 0):= "00011";
+  constant S6: STD_LOGIC_VECTOR(4 downto 0) := "00110";
+  constant S14: STD_LOGIC_VECTOR(4 downto 0) := "01110";
+  constant S19: STD_LOGIC_VECTOR(4 downto 0) := "10011";
+  constant S21: STD_LOGIC_VECTOR(4 downto 0) := "10101";
+  constant S24: STD_LOGIC_VECTOR(4 downto 0) := "11000";
+  constant S27: STD_LOGIC_VECTOR(4 downto 0) := "11011";
+  constant S31: STD_LOGIC_VECTOR(4 downto 0) := "11111";
   
 
   begin
