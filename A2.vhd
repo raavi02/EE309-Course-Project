19d3fb68341c31f959165bf30e47d94f13dbbc54
Srishti Sharma <81479041+srishtis19@users.noreply.github.com>
2022-04-17 18:23:40 +0530

    encoding change A2

--- A2.vhd
+++ A2.vhd
@@ -6,17 +6,17 @@ use work.all;
 entity A2 is
 	port (ir6_8 : IN STD_LOGIC_VECTOR(2 downto 0);
 				Count : IN STD_LOGIC_VECTOR(2 downto 0);
-				current_state : IN STD_LOGIC_VECTOR(5 downto 0);
+				current_state : IN STD_LOGIC_VECTOR(4 downto 0);
 
 				output : OUT STD_LOGIC_VECTOR(2 downto 0));
 end A2;
 
 
 architecture beh of A2 is
-  constant S3: STD_LOGIC_VECTOR(5 downto 0) := "000011";
-  constant S6: STD_LOGIC_VECTOR(5 downto 0) := "000110";
-  constant S14: STD_LOGIC_VECTOR(5 downto 0) := "001110";
-  constant S25: STD_LOGIC_VECTOR(5 downto 0) := "011001";
+  constant S3 : std_logic_vector(4 downto 0):= "00011";
+  constant S6: STD_LOGIC_VECTOR(4 downto 0) := "00110";
+  constant S14: STD_LOGIC_VECTOR(4 downto 0) := "01110";
+  constant S25: STD_LOGIC_VECTOR(4 downto 0) := "11001";
   
 
   begin
